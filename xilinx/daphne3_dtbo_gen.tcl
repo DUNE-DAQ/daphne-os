b197fb97ac251ac98ba65a0605a371d20cc6a792
danavilag <daniel.avila@eia.edu.co>
2025-09-18 14:58:01 -0500

    Added full support for Linux, now the design generates all the necessary files to program the board without manually generating them, added and changed files accordingly. Also, fixed a small bug that would not let the script recognize it was working on Windows, and added a small correction to the README file.

--- xilinx/daphne3_dtbo_gen.tcl
+++ xilinx/daphne3_dtbo_gen.tcl
@@ -1,27 +1,21 @@
 # simple TCL code to generate Device Tree Overlay for Petalinux
 # this code must run inside the XSCT Vitis environment
 # call for this specific process to run first
- 
-# check if XSCT is callable
-if {[catch {exec xsct -h} result]} {
-    puts "ERROR: XSCT not found in PATH."
-    puts "Please make sure Vitis is installed and XSCT is in your PATH."
-    exit 1
-} else {
-    puts "INFO: XSCT found and available."
-}
- 
+
 # define the hardware description files
 set hw      [lindex $argv 0]
- 
+
 # define output directory
 set out_dir [lindex $argv 1]
- 
+
 # receive the git commit number
 set git_sha [lindex $argv 2]
  
+# open the generated hardware design XSA
+hsi::open_hw_design $out_dir/daphne3_$git_sha.xsa
+
 # generate the device tree using the generated XSA
-createdts -hw $hw -out $out_dir -platform-name daphne3_$git_sha -overlay
- 
+createdts -hw $hw -zocl -platform-name daphne3_$git_sha -git-branch xlnx_rel_v2022.2 -overlay -compile -out $out_dir/daphne3_$git_sha 
+
 # exit the process once done
 exit
